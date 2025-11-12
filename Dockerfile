564fb279fa0a9e55f3dd88949869127c832bafee
Shashi Shekhar <shshekhar93@gmail.com>
2025-11-12 21:19:31 +0000

    Proxy implemented

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,17 @@
 FROM node:lts-alpine
-RUN apk add git bash nginx
+RUN apk add git bash curl nginx
 
-ARG DUMBSTACK_DOMAIN
+ARG DUMBSTACK_DOMAIN="dumb.local"
 
 EXPOSE 8080
 
-COPY dumbstack.conf /etc/nginx/http.d/
 COPY bootstrap.sh /tmp/
 RUN chmod +x /tmp/bootstrap.sh
-RUN /tmp/bootstrap.sh ${DUMBSTACK_DOMAIN}
+RUN /tmp/bootstrap.sh
+
+RUN rm /etc/nginx/http.d/default.conf
+COPY dumbstack.conf /etc/nginx/http.d/
+RUN sed -i "15i server_name ${DUMBSTACK_DOMAIN}" /etc/nginx/http.d/dumbstack.conf
 
 COPY start.sh /usr/bin/
 RUN chmod +x /usr/bin/start.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-# nginx stuff
-# domainName=$1
-# echo "Domain is $domainName"
-# sed -i "9i server_name $domainName" /etc/nginx/http.d/dumbstack.conf
-# rm /etc/nginx/http.d/default.conf
-
 # Configure list of apps and its repo
-DUMB_PAD_REPO=https://github.com/DumbWareio/DumbPad
-DUMB_DROP_REPO=https://github.com/DumbWareio/DumbDrop
-DUMB_BUDGET_REPO=https://github.com/DumbWareio/DumbBudget
-DUMB_TERM_REPO=https://github.com/DumbWareio/DumbTerm
-DUMB_DO_REPO=https://github.com/DumbWareio/DumbDo
-DUMB_ASSETS_REPO=https://github.com/DumbWareio/DumbAssets
-DUMB_KAN_REPO=https://github.com/DumbWareio/DumbKan
-DUMB_WHOIS_REPO=https://github.com/DumbWareio/DumbWhoIs
-
 declare -A APPS=(
   [DumbPad]='https://github.com/DumbWareio/DumbPad'
   [DumbDrop]='https://github.com/DumbWareio/DumbDrop'
